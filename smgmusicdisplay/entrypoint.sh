11545e66eecc882f799645f1a1ddfdd8c7d8ba0a
laura <mail@laura.fm>
2020-10-17 08:08:34 +0200

    Add: Only sync vendor directory in code image

--- smgmusicdisplay/entrypoint.sh
+++ smgmusicdisplay/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "syncing"
-rsync -au /var/wwww/ /var/www
+rsync -au /var/wwww/vendor /var/www/vendor
 echo "synced, changing permissions"
 chown -R laradock:laradock /var/www
 echo "Done."
